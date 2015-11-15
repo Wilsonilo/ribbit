0ca0c2ea2a36f8ca90c3d090e95782ad7f287cf7
Wilson Mu√±oz <contacto@maquilaweb.net>
2015-11-15 11:44:51 -0500

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Wilson Muñoz on 11/15/15.
+//  Copyright © 2015 Wilson Muñoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
